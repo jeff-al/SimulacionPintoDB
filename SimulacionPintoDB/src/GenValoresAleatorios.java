16be1f240de886912b91cbb1b8fc0a025604d3bf
Jefferson Álvarez <jeffalvarez08@gmail.com>
2018-06-18 13:38:01 -0600

    Funciones de Variables Aleatorias Hechas

--- SimulacionPintoDB/src/GenValoresAleatorios.java
+++ SimulacionPintoDB/src/GenValoresAleatorios.java
@@ -4,14 +4,24 @@ public class GenValoresAleatorios {
     double result;
 
     double GenerarValNormal(double varianza, double media) {
+        double sumRandom = 0;
+        for (int i = 0; i < 12; i++) {
+            sumRandom += Math.random();
+        }
+        double Z = sumRandom - 6;
+        result = media + (Math.sqrt(varianza) * Z);
         return result;
     }
 
     double GenerarValExponencial(double media) {
+        double random = Math.random();
+        result = ((-1) / media) * Math.log(random);
         return result;
     }
 
-    double GenerarValUniforme() {
+    double GenerarValUniforme(double a, double b) {
+        double random = Math.random();
+        result = a + ((b - a) * random);
         return result;
     }
 }
